df864e37e790268135637a3aeb03323fed287731
Umit Kabuli <umitkabuli@hotmail.com>
2019-02-27 02:44:57 +0300

    updated

--- bashscripts/bashtools.sh
+++ bashscripts/bashtools.sh
@@ -4,3 +4,24 @@ getresulation()
 {
   xdpyinfo  | grep -oP 'dimensions:\s+\K\S+'
 }
+
+getdistribitorid()
+{
+  lsb_release -i|awk '{print $3}'
+}
+
+getfullversion()
+{
+  lsb_release -v|awk '{print $3}'
+}
+
+getrelase()
+{
+  lsb_release -r|awk '{print $2}'
+}
+
+getcodename()
+{
+  lsb_release -c|awk '{print $2}'
+}
+
